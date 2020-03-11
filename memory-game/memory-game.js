4db64708d266d2c06598245827e92558196c01f4
Azrael <46539272+shady-plato@users.noreply.github.com>
2020-03-11 20:48:54 +0000

    fixed memory game match filename

--- memory-game/memory-game.js
+++ memory-game/memory-game.js
@@ -41,7 +41,7 @@ for(i=0; i<buttons.length; i++) {
                 if(e.target.dataset.number == lastKnownButtonNumber) {
                     
                     e.target.style.backgroundImage = "url('memory-game/resources/background_Match.jpg')";
-                    document.getElementById(lastKnownButtonId).style.backgroundImage="url('memory-game/resources/background_Match.jpg')";
+                    document.getElementById(lastKnownButtonId).style.backgroundImage="url('memory-game/resources/background_match.jpg')";
                     lastKnownButtonId = undefined;
                     lastKnownButtonNumber = undefined;
                     matches++;
